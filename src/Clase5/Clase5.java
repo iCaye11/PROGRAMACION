5cb1f5831de4d8a42c481851a7c42d6444535d7b
iCaye11 <cayetanogon@gmail.com>
2025-03-23 14:14:01 +0100

    .

---
+++ src/Clase5/Clase5.java
@@ -0,0 +1,110 @@
+package Clase5;
+
+import java.util.Scanner;
+//Sin esto, no puedo pedirle al usuario ningún dato
+public class Clase5 {
+    public static void main(String[] args) {
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Dame un número: ");
+//        int numero = scanner.nextInt();//Con esto, leo el numero del usuario
+//
+//        //Estructura del if-else
+//        if (numero > 0) {
+//            System.out.println("El número es positivo");
+//        } else if (numero < 0) {
+//            System.out.println("El numero es negativo");
+//        } else {
+//            System.out.println("El numero es cero");
+//        }
+//        scanner.close();//Cierra el escaner.
+//
+//        //Menu de opciones donde se elije
+//
+//        System.out.println("*Menú de opciones*");
+//        System.out.println("1. Saludar");
+//        System.out.println("2. Mostrar fecha");
+//        System.out.println("3. Salir");
+//
+//        System.out.println("Seleciona una opción");
+//          int opcion = scanner.nextInt();//Leemos la opcion del ususario
+//
+//        //Evaluar opciones con el switch
+//        switch (opcion) {
+//            case 1:
+//                System.out.println("Hola, bienvenido");
+//                break;
+//            case 2:
+//                System.out.println("Bro, this is massive!!");
+//                break;
+//            case 3:
+//                System.out.println("Salir del programa");
+//                break;
+//            case 4:
+//                System.out.println("Macho, no lo entiendo");
+//        }
+//         scanner.close();
+//
+//        Usar bucle para contar
+//        for (int i = 1 ; i <= 10; i++) {
+//            System.out.println("Numero:  " + i);
+//            //Así muestro cada interacción
+//
+//        }
+//        Ejercicio 4, usar el while para PEDIR numeros. Siempre que evamos pedir=scanner
+//        Scanner  scanner = new Scanner(System.in);
+//        int numero;
+//        System.out.println("Dame un numero y pon 0 para salir");
+//        numero = scanner.nextInt();
+//        //la idea es que mientars no sea cero, me siga pidiendo numeros
+//        while (numero!=0){
+//            System.out.println("Has introducido: " + numero);
+//            System.out.println("Introduce otro numero: ");
+//            numero = scanner.nextInt();
+//
+//        }
+//        System.out.println("Programa finalizado");
+//        scanner.close();
+//
+//        Ejercicio 5. Pide contraseña y que no pueda entarra el usuario hasta que ponga java123 (do-while)
+//        //PEDIR SUPONE IMPORT SCANNER
+//        Scanner scanner = new Scanner(System.in);
+//        String password;
+//
+//        //Bucle do-while
+//        do {
+//            System.out.println("Introduce tu contraseña: ");
+//            password = scanner.nextLine();
+//        }while(!password.equals("java123"));
+//
+//        System.out.println("Acceso concedido");
+//        scanner.close();
+
+        //Imprimir numeros del 1 al 10 con un for
+        //Usar bucle para contar del 1 al 10
+//        for (int i=1;i<=10;i++){
+//            System.out.println("Numero: " + i);
+//        }
+
+        Scanner scanner = new Scanner(System.in);
+        int numero;
+        System.out.println("Dame un numero. El 0 es para salir.");
+        numero = scanner.nextInt();
+        while(numero!=0){
+            System.out.println("Has introducido: "+ numero);
+            System.out.println("Dame otro número: ");
+            numero = scanner.nextInt();
+
+        }
+        System.out.println("Programa finalizado");
+        scanner.close();
+
+
+
+
+    }
+}
+
+
+
+
